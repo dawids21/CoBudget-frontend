8aa6e9dcd6ad0081739cf1f0303de43df2bf441b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-15 20:26:13 +0200

    add basic structure to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
+  <v-app>
+    <TopNav/>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app>
 
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import TopNav from '@/components/TopNav'
 
 export default {
   name: 'App',
+  components: {TopNav},
 }
 </script>
