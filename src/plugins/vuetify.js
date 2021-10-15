ebd9d010bfb852931a588951e9e23f2edf527083
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-15 20:25:33 +0200

    add colors to Vuetify object

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+    theme: {
+        themes: {
+            light: {
+                primary: '#2E7C31',
+                secondary: '#880E4F',
+                accent: '#60AC5D',
+                error: '#FF5252',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107',
+            },
+        },
+    },
+})
