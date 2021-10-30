8d892b1d30eba4f231dccdd2241cbfaff9ce8572
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-30 16:03:39 +0200

    copy Login view from tutorial

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container fluid class="login">
+    <div id="okta-signin-container"></div>
+  </v-container>
+</template>
+
+<script>
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
+import {oktaSignIn} from '@/okta'
+
+export default {
+  name: 'Login',
+  mounted: function () {
+    this.$nextTick(function () {
+      oktaSignIn.showSignInAndRedirect(
+          {el: '#okta-signin-container'},
+      )
+    })
+  },
+  unmounted() {
+    oktaSignIn.remove()
+  },
+}
+</script>
