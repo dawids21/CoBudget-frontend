b5289e5aa265106d6f9180ae8ed423096a90228f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-10-30 23:53:26 +0200

    add config for Okta in main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import OktaVue from '@okta/okta-vue'
+import {oktaAuth} from '@/okta'
 
 Vue.config.productionTip = false
+Vue.use(OktaVue, {
+    oktaAuth,
+    onAuthRequired: () => router.push('/login'),
+})
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
+    router,
+    vuetify,
+    render: h => h(App),
 }).$mount('#app')
