211c21773b1b6db9745f3c23a4b105329a5c9c6c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-01 20:41:54 +0100

    add basic submitting of entry form

--- src/components/calendar/AddEntryDialog.vue
+++ src/components/calendar/AddEntryDialog.vue
@@ -6,21 +6,28 @@
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-radio-group row mandatory>
+          <v-radio-group v-model="type" row mandatory>
             <v-radio label="Expense" color="secondary" value="expense"/>
             <v-radio label="Income" color="primary" value="income"/>
           </v-radio-group>
-          <v-text-field type="number" label="Amount"/>
-          <v-text-field type="date" label="Date"/>
-          <v-select :items="categories" item-value="id" item-text="name" label="Category"/>
-          <v-text-field label="Subcategory"/>
+          <v-text-field v-model="amount" type="number" label="Amount"/>
+          <v-text-field v-model="date" type="date" label="Date"/>
+          <v-select v-model="category" :items="categories" item-value="id" item-text="name" label="Category"/>
+          <v-text-field v-model="subcategory" label="Subcategory"/>
         </v-form>
       </v-card-text>
+      <v-card-actions>
+        <v-btn color="secondary" @click.stop="show = false">Close</v-btn>
+        <v-spacer/>
+        <v-btn color="primary" type="submit" @click="addEntry">Add</v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "AddEntryDialog",
   props: {
@@ -33,6 +40,11 @@ export default {
         {id: 2, name: "Clothes"},
         {id: 3, name: "House"},
       ],
+      type: "expense",
+      amount: 0,
+      date: new Date().toISOString().split('T')[0],
+      category: null,
+      subcategory: "",
     }
   },
   computed: {
@@ -43,6 +55,22 @@ export default {
       set(value) {
         this.$emit('input', value)
       },
+    },
+  },
+  methods: {
+    async addEntry() {
+      const accessToken = this.$auth.getAccessToken()
+      await axios.post(
+          'http://localhost:8081/api/entry',
+          {
+            amount: this.type === 'expense' ? -Math.abs(this.amount) : Math.abs(this.amount),
+            date: this.date,
+            categoryId: this.category,
+            subcategory: this.subcategory,
+          },
+          {headers: {Authorization: `Bearer ${accessToken}`}},
+      )
+      this.show = false
     },
   },
 }
