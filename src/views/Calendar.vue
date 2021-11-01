d2178dad4a0f677cd77004a5ddc90d248473ce00
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-01 18:46:20 +0100

    add floating action button to calendar view

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,7 +1,22 @@
 <template>
-  <v-container fluid>
-    <Week :day="new Date()"/>
-  </v-container>
+  <div>
+    <v-container fluid>
+      <Week :day="new Date()"/>
+    </v-container>
+
+    <v-btn
+        class="ma-4"
+        color="primary"
+        fab
+        large
+        fixed
+        bottom
+        right
+        @click.stop="openAddEntryDialog"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
@@ -10,6 +25,12 @@ import Week from '@/components/calendar/Week'
 export default {
   name: "Calendar",
   components: {Week},
+
+  methods: {
+    openAddEntryDialog() {
+      console.log("open")
+    },
+  },
 }
 </script>
 
