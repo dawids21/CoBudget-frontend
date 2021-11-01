6d7b7aa232be8497eece187fb32b1f75766ac489
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-01 20:09:28 +0100

    add necessary input fields to entry form

--- src/components/calendar/AddEntryDialog.vue
+++ src/components/calendar/AddEntryDialog.vue
@@ -1,6 +1,22 @@
 <template>
   <v-dialog v-model="show">
-    Add entry
+    <v-card>
+      <v-card-title>
+        Add entry
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-radio-group row mandatory>
+            <v-radio label="Expense" color="secondary" value="expense"/>
+            <v-radio label="Income" color="primary" value="income"/>
+          </v-radio-group>
+          <v-text-field type="number" label="Amount"/>
+          <v-text-field type="date" label="Date"/>
+          <v-select :items="categories" item-value="id" item-text="name" label="Category"/>
+          <v-text-field label="Subcategory"/>
+        </v-form>
+      </v-card-text>
+    </v-card>
   </v-dialog>
 </template>
 
@@ -9,6 +25,15 @@ export default {
   name: "AddEntryDialog",
   props: {
     value: Boolean,
+  },
+  data() {
+    return {
+      categories: [
+        {id: 1, name: "Food"},
+        {id: 2, name: "Clothes"},
+        {id: 3, name: "House"},
+      ],
+    }
   },
   computed: {
     show: {
