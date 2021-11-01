61e006637d4dde22b80d94c8c82c5f7957c9b390
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-01 18:46:43 +0100

    create component AddEntryDialog.vue

---
+++ src/components/calendar/AddEntryDialog.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-dialog v-model="show">
+    Add entry
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "AddEntryDialog",
+  props: {
+    show: Boolean,
+  },
+}
+</script>
+
+<style>
+
+</style>
