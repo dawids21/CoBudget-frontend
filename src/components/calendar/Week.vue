b7b27d783838ea15ff09d2a78d217779229984ab
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-12 07:28:58 +0100

    move data from Week to Calendar

--- src/components/calendar/Week.vue
+++ src/components/calendar/Week.vue
@@ -8,54 +8,14 @@
 
 <script>
 import DayCard from '@/components/calendar/DayCard'
-import axiosInstance from '@/config/axios'
-
-function getStartDate(day) {
-  return new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate() - (day.getDay() + 6) % 7))
-}
-
-async function getEntries() {
-  const end = new Date(this.start.getTime())
-  end.setDate(this.start.getDate() + 6)
-  const response = await axiosInstance.get('/api/entry', {
-    params: {
-      from: this.start.toISOString().split('T')[0],
-      to: end.toISOString().split('T')[0],
-    },
-  })
-  return response.data.map(item => {
-    return {
-      amount: item.amount,
-      date: new Date(item.date),
-      category: item.category ?? "Unknown",
-      subcategory: item.subcategory,
-    }
-  })
-}
 
 export default {
   name: "Week",
   components: {DayCard},
   props: {
     day: Date,
-  },
-  data() {
-    return {
-      start: getStartDate(this.day),
-      entries: [],
-    }
-  },
-  computed: {
-    days() {
-      return [0, 1, 2, 3, 4, 5, 6].map(n => {
-        const clone = new Date(this.start.getTime())
-        clone.setDate(clone.getDate() + n)
-        return clone
-      })
-    },
-  },
-  mounted: async function () {
-    this.entries = await getEntries.call(this)
+    days: Array,
+    entries: Array,
   },
 }
 </script>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <Week :day="new Date()"/>
+    <Week :day="new Date()" :days="days" :entries="entries"/>
     <v-btn
         class="ma-4"
         color="primary"
@@ -21,6 +21,7 @@
 <script>
 import Week from '@/components/calendar/Week'
 import AddEntryDialog from '@/components/calendar/AddEntryDialog'
+import axiosInstance from '@/config/axios'
 
 export default {
   name: "Calendar",
@@ -28,13 +29,51 @@ export default {
   data() {
     return {
       showDialog: false,
+      start: this.getStartDate(new Date()),
+      entries: [],
     }
+  },
+
+  computed: {
+    days() {
+      return [0, 1, 2, 3, 4, 5, 6].map(n => {
+        const clone = new Date(this.start.getTime())
+        clone.setDate(clone.getDate() + n)
+        return clone
+      })
+    },
   },
 
   methods: {
     openAddEntryDialog() {
       this.showDialog = true
     },
+
+    getStartDate(day) {
+      return new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate() - (day.getDay() + 6) % 7))
+    },
+
+    async getEntries() {
+      const end = new Date(this.start.getTime())
+      end.setDate(this.start.getDate() + 6)
+      const response = await axiosInstance.get('/api/entry', {
+        params: {
+          from: this.start.toISOString().split('T')[0],
+          to: end.toISOString().split('T')[0],
+        },
+      })
+      return response.data.map(item => {
+        return {
+          amount: item.amount,
+          date: new Date(item.date),
+          category: item.category ?? "Unknown",
+          subcategory: item.subcategory,
+        }
+      })
+    },
+  },
+  mounted: async function () {
+    this.entries = await this.getEntries()
   },
 }
 </script>
