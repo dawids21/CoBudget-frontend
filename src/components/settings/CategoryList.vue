a3583c861470c5b3a34cdfbc65ea40c6bdc11ff3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-14 18:31:54 +0100

    display categories and subcategories as tree

--- src/components/settings/CategoryList.vue
+++ src/components/settings/CategoryList.vue
@@ -1,10 +1,53 @@
 <template>
-  <h1>Categories</h1>
+  <v-list>
+    <v-list-group v-for="category in categories" :key="category.id" no-action prepend-icon="mdi-circle-medium">
+      <template v-slot:activator>
+        <v-list-item-title class="text-h6" v-text="category.name"></v-list-item-title>
+      </template>
+      <v-list-item v-for="subcategory in category.subcategories" :key="subcategory.id">
+        <v-list-item-title class="text-subtitle-1" v-text="subcategory.name"></v-list-item-title>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
 </template>
 
 <script>
 export default {
   name: "CategoryList",
+  data() {
+    return {
+      categories: [
+        {
+          id: 1,
+          name: 'Food',
+          subcategories: [
+            {
+              id: 3,
+              name: 'Home',
+            },
+            {
+              id: 4,
+              name: 'Work',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Clothes',
+          subcategories: [
+            {
+              id: 5,
+              name: 'Shirts',
+            },
+            {
+              id: 6,
+              name: 'Shoes',
+            },
+          ],
+        },
+      ],
+    }
+  },
 }
 </script>
 
