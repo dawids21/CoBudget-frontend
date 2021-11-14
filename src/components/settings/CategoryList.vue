8bfb380c37faa6b826688db94b451571a6e450af
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-14 19:49:19 +0100

    implement functions to add new categories

--- src/components/settings/CategoryList.vue
+++ src/components/settings/CategoryList.vue
@@ -7,7 +7,21 @@
       <v-list-item v-for="subcategory in category.subcategories" :key="subcategory.id">
         <v-list-item-title class="text-subtitle-1" v-text="subcategory.name"></v-list-item-title>
       </v-list-item>
+      <v-list-item>
+        <v-list-item-title>
+          <v-text-field v-model="newSubcategory" label="Add subcategory"
+                        @focusout="newSubcategory = null" @keyup.enter="addSubcategory(category.id)"></v-text-field>
+        </v-list-item-title>
+      </v-list-item>
     </v-list-group>
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon>mdi-plus</v-icon>
+      </v-list-item-icon>
+      <v-list-item-title>
+        <v-text-field v-model="newCategory" label="Add category" @keyup.enter="addCategory"></v-text-field>
+      </v-list-item-title>
+    </v-list-item>
   </v-list>
 </template>
 
@@ -16,7 +30,30 @@ export default {
   name: "CategoryList",
   data() {
     return {
-      categories: [
+      categories: this.getCategories(),
+      newCategory: null,
+      newSubcategory: null,
+    }
+  },
+  methods: {
+    addCategory() {
+      if (!this.newCategory) {
+        return
+      }
+      console.log(`name: ${this.newCategory}`)
+      this.newCategory = null
+      this.getCategories()
+    },
+    addSubcategory(parentId) {
+      if (!this.newSubcategory) {
+        return
+      }
+      console.log(`name: ${this.newSubcategory}, parentId: ${parentId}`)
+      this.newSubcategory = null
+      this.getCategories()
+    },
+    getCategories() {
+      return [
         {
           id: 1,
           name: 'Food',
@@ -45,8 +82,10 @@ export default {
             },
           ],
         },
-      ],
-    }
+      ]
+    },
+
+
   },
 }
 </script>
