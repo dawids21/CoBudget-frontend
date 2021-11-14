f80f35e67afd3d3b24c8d791df98df3c18bbbb73
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-14 20:21:59 +0100

    implement view for larger screens

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-    <v-expansion-panels>
+    <v-expansion-panels class="hidden-md-and-up">
       <v-expansion-panel>
         <v-expansion-panel-header class="text-h6">Categories</v-expansion-panel-header>
         <v-expansion-panel-content>
@@ -8,6 +8,22 @@
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
+    <v-row class="hidden-sm-and-down" justify="center">
+      <v-col lg="4">
+        <v-card height="100%">
+          <v-card-title>Categories</v-card-title>
+          <v-card-text>
+            <CategoryList/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col lg="4">
+        <v-card height="100%">
+          <v-card-title>Accounts</v-card-title>
+          <v-card-text>Default</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
