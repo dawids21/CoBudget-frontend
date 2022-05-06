0079a082d33e45e4e6516fae911c8edad2a2b326
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-06 11:17:56 +0200

    create basic structure for component

--- src/components/calendar/MonthAndYear.vue
+++ src/components/calendar/MonthAndYear.vue
@@ -1,10 +1,20 @@
 <template>
-
+<div>
+  <p>{{this.toDisplay}}</p>
+</div>
 </template>
 
 <script>
 export default {
   name: "MonthAndYear",
+  props: {
+    date: Date
+  },
+  computed: {
+    toDisplay() {
+      return "May 2022"
+    }
+  }
 }
 </script>
 
