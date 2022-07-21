8cefe7d7afc093776a3a0b3388fecbb8631ab81b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-21 17:14:58 +0200

    feat: Add calendar page to routes

--- src/components/Routes/AppRoutes.js
+++ src/components/Routes/AppRoutes.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Calendar from "../../pages/Calendar";
 import Home from "../../pages/Home";
+import AppSecureRoute from "./AppSecureRoute";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/calendar" element={<AppSecureRoute />}>
+        <Route path="" element={<Calendar />} />
+      </Route>
     </Routes>
   );
 };
