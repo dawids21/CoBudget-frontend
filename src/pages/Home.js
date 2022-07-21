a7cbd9712499ecf77cf8021cb894108474f02a19
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-21 17:18:07 +0200

    feat: Change home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,11 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 const Home = () => {
-  return <Typography>Let's get started!</Typography>;
+    return (
+      <Typography variant="h1" component="h1">
+        Home
+      </Typography>
+    );
 };
 
 export default Home;
