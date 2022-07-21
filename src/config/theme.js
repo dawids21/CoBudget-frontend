75f10f4dd511cdf2da7c43025c77799552f66f5b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-21 17:50:34 +0200

    feat: Copy theme from Vue app

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,5 +1,27 @@
 import { createTheme } from "@mui/material";
+import { green, pink } from "@mui/material/colors";
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[800],
+    },
+    secondary: {
+      main: pink[900],
+    },
+    error: {
+      main: "#ff5252",
+    },
+    info: {
+      main: "#2196f3",
+    },
+    success: {
+      main: "#4caf50",
+    },
+    warning: {
+      main: "#ffc107",
+    },
+  },
+});
 
 export default theme;
