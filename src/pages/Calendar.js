66559a2dce4f61888e626b20901f08d054ab9479
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-21 17:05:42 +0200

    feat: Create calendar dummy page

---
+++ src/pages/Calendar.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Calendar = () => {
+  return (
+    <Typography variant="h1" component="h1">
+      Calendar
+    </Typography>
+  );
+};
+
+export default Calendar;
