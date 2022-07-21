8e37475541be773a3be420985b653776832d669b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-21 16:56:54 +0200

    feat: Add okta auth to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { Security } from "@okta/okta-react";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import AppRoutes from "./components/Routes/AppRoutes";
+import config from "./config";
 
-function App() {
-  return <AppRoutes />;
-}
+const oktaAuth = new OktaAuth(config.oidc);
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const restoreOriginalUri = useCallback((_oktaAuth, originalUri) => {
+    navigate(toRelativeUrl(originalUri || "/", window.location.origin));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <main>
+        <AppRoutes />
+      </main>
+    </Security>
+  );
+};
 
 export default App;
