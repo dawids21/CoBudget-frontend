6c4d3c98626e09670cf35d88eef42957b4c00f1e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-29 19:16:34 +0200

    feat: Create mock category list

---
+++ src/components/UI/CollapseList/CollapseList.js
@@ -0,0 +1,57 @@
+import React, { useCallback, useState } from "react";
+
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import {
+  Collapse,
+  List,
+  ListItemButton,
+  ListItemText,
+  Box,
+} from "@mui/material";
+
+const CollapseList = (props) => {
+  const { data } = props;
+  const initialState = {};
+  for (const list in data) {
+    initialState[list.name] = false;
+  }
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const clickHandler = (name) => {
+    setIsOpen((prev) => {
+      const newState = { ...prev };
+      newState[name] = !newState[name];
+      return newState;
+    });
+  };
+
+  const getSubListComponent = useCallback((subList) => {
+    return (
+      <ListItemButton key={subList.id} sx={{ pl: 4 }}>
+        <ListItemText primary={subList.name} />
+      </ListItemButton>
+    );
+  }, []);
+
+  const getListComponent = useCallback((list) => {
+    const subLists = list.sub;
+    return (
+      <Box key={list.id}>
+        <ListItemButton onClick={clickHandler.bind(null, list.name)}>
+          <ListItemText primary={list.name} />
+          {isOpen[list.name] ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={isOpen[list.name]} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {subLists.map(getSubListComponent)}
+          </List>
+        </Collapse>
+      </Box>
+    );
+  }, []);
+
+  return <List>{data.map(getListComponent)}</List>;
+};
+
+export default CollapseList;
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,7 +1,93 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import CollapseList from "../components/UI/CollapseList/CollapseList";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editHandler = () => {
+    setIsEditing(true);
+  };
+
+  const cancelHandler = () => {
+    setIsEditing(false);
+  };
+
+  const confirmHandler = () => {
+    setIsEditing(false);
+  };
+
+  const listData = [
+    {
+      id: 1,
+      name: "Food",
+      sub: [
+        {
+          id: 2,
+          name: "Home",
+        },
+        {
+          id: 3,
+          name: "Work",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Income",
+      sub: [
+        {
+          id: 5,
+          name: "Work",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <Box sx={{ mt: 2, mx: 4, textAlign: "center" }}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Card elevation={3}>
+            <CardContent>
+              <Typography component="h2" variant="h4">
+                Categories
+              </Typography>
+              <CollapseList data={listData} />
+            </CardContent>
+            {isEditing ? (
+              <CardActions sx={{ justifyContent: "space-between" }}>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  onClick={cancelHandler}
+                >
+                  Cancel
+                </Button>
+                <Button variant="contained" onClick={confirmHandler}>
+                  Confirm
+                </Button>
+              </CardActions>
+            ) : (
+              <CardActions sx={{ justifyContent: "flex-end" }}>
+                <Button variant="outlined" onClick={editHandler}>
+                  Edit
+                </Button>
+              </CardActions>
+            )}
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Settings;
