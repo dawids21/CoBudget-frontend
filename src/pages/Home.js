55a31ba03e43d994cffb37489eadf5fd439b0091
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-07-29 18:15:37 +0200

    fix: Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,11 +9,10 @@ const Home = () => {
     <Typography
       sx={{
         position: "absolute",
-        top: "50%",
         left: "50%",
-        transform: "translate(-50%, -50%)",
+        top: "50%",
+        transform: "translate(-50%,-50%)",
       }}
-      align="center"
       variant="h2"
       component="h1"
     >
