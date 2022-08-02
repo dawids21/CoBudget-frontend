e0730587d31b8aca3a45a1db02851e07b44fc720
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-08-02 19:04:56 +0200

    feat: Show info when plan is empty

--- src/components/Plan/PlanInfo.js
+++ src/components/Plan/PlanInfo.js
@@ -75,7 +75,13 @@ const PlanInfo = (props) => {
 
   return (
     <Paper elevation={3}>
-      <List>{plan.map(getListComponent)}</List>
+      {plan.length !== 0 ? (
+        <List>{plan.map(getListComponent)}</List>
+      ) : (
+        <Typography sx={{ py: 2 }} variant="h3" component="h3">
+          Plan is empty
+        </Typography>
+      )}
     </Paper>
   );
 };
