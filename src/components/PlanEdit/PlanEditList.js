3e1ea8737cd93324c487d42e09dd5563e6f8ba23
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-08-03 22:10:54 +0200

    feat: Handle changing input

--- src/components/PlanEdit/PlanEditList.js
+++ src/components/PlanEdit/PlanEditList.js
@@ -15,6 +15,24 @@ import {
 } from "@mui/material";
 
 const planReducer = (state, action) => {
+  if (action.type === "CHANGE_VALUE") {
+    const copyOfState = [...state].map((category) => ({
+      ...category,
+      subcategories: [...category.subcategories],
+    }));
+    const category = copyOfState.find(
+      (category) => category.id === action.categoryId
+    );
+    const subcategoryIndex = category.subcategories.findIndex(
+      (subcategory) => subcategory.id === action.subcategoryId
+    );
+    const subcategory = category.subcategories[subcategoryIndex];
+    category.subcategories[subcategoryIndex] = {
+      ...subcategory,
+      amount: action.value,
+    };
+    return copyOfState;
+  }
   return state;
 };
 
@@ -34,10 +52,8 @@ const PlanEditList = (props) => {
           ...subcategory,
           amount: "",
         }));
-        return { ...category, amount: 0, subcategories };
+        return { ...category, subcategories };
       }
-
-      let categoryAmount = 0;
 
       const subcategories = category.subcategories.map((subcategory) => {
         const plannedSubcategory = plannedCategory.plannedSubcategories.find(
@@ -48,12 +64,10 @@ const PlanEditList = (props) => {
           return { ...subcategory, amount: "" };
         }
 
-        categoryAmount += plannedSubcategory.amount;
-
         return { ...subcategory, amount: `${plannedSubcategory.amount}` };
       });
 
-      return { ...category, amount: categoryAmount, subcategories };
+      return { ...category, subcategories };
     })
   );
 
@@ -70,29 +84,26 @@ const PlanEditList = (props) => {
     });
   };
 
-  const getSubListComponent = (subcategory) => {
-    return (
-      <ListItem key={subcategory.id} sx={{ pl: 4 }}>
-        <ListItemText primary={subcategory.name} />
-        <TextField
-          value={subcategory.amount}
-          sx={{ mr: 1, width: "10ch" }}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">$</InputAdornment>,
-          }}
-        />
-      </ListItem>
-    );
+  const changeSubcategoryHandler = (categoryId, subcategoryId, event) => {
+    dispatchPlan({
+      type: "CHANGE_VALUE",
+      categoryId,
+      subcategoryId,
+      value: event.target.value,
+    });
   };
 
   const getListComponent = (category) => {
+    const categoryAmount = category.subcategories
+      .map((subcategory) => subcategory.amount)
+      .reduce((current, value) => current + (value ? parseInt(value) : 0), 0);
     return (
       <Box key={category.id}>
         <ListItem disablePadding>
           <ListItemButton onClick={clickHandler.bind(null, category.name)}>
             <ListItemText primary={category.name} />
             <Typography variant="h6" sx={{ mr: 1 }}>
-              {category.amount}$
+              {categoryAmount}$
             </Typography>
             {isOpen.includes(category.name) ? (
               <ExpandLessIcon />
@@ -107,10 +118,33 @@ const PlanEditList = (props) => {
           unmountOnExit
         >
           <List component="div" disablePadding>
-            {category.subcategories.map(getSubListComponent)}
+            {category.subcategories.map(
+              getSubListComponent.bind(null, category.id)
+            )}
           </List>
         </Collapse>
       </Box>
+    );
+  };
+
+  const getSubListComponent = (categoryId, subcategory) => {
+    return (
+      <ListItem key={subcategory.id} sx={{ pl: 4 }}>
+        <ListItemText primary={subcategory.name} />
+        <TextField
+          type="number"
+          value={subcategory.amount}
+          onChange={changeSubcategoryHandler.bind(
+            null,
+            categoryId,
+            subcategory.id
+          )}
+          sx={{ mr: 1, width: "10ch" }}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">$</InputAdornment>,
+          }}
+        />
+      </ListItem>
     );
   };
 
