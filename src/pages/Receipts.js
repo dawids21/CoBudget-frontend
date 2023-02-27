1ced7a0594fb4daf943216179b1cca967ff36f26
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-02-27 18:44:42 +0100

    feat: Add component for ready soon page

--- src/pages/Receipts.js
+++ src/pages/Receipts.js
@@ -1,13 +1,22 @@
 import React, {useEffect} from "react";
-import {Typography} from "@mui/material";
+import UnderDevelopment from "../components/UI/UnderDevelopment";
 
 const Receipts = () => {
-    useEffect(() => {
-        document.title = "CoBudget - receipts";
-    }, []);
-    return (
-        <Typography>Receipts</Typography>
-    );
+  useEffect(() => {
+    document.title = "CoBudget - receipts";
+  }, []);
+  return (
+    <UnderDevelopment
+      sx={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%,-50%)",
+        textAlign: "center"
+      }}
+      variant="h4"
+    />
+  );
 };
 
 export default Receipts;
